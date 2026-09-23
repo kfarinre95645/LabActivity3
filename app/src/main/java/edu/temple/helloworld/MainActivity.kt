fe5a95e1db03b177fcc26c9140f1e44879f3648f
Kenny Farinre <tur95645@temple.edu>
2026-09-23 16:07:06 -0400

    feature-1

--- app/src/main/java/edu/temple/helloworld/MainActivity.kt
+++ app/src/main/java/edu/temple/helloworld/MainActivity.kt
@@ -20,9 +20,15 @@ class MainActivity : AppCompatActivity() {
 
         
         findViewById<Button>(R.id.clickMeButton).setOnClickListener {
-            displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            if (findViewById<EditText>(R.id.nameEditText).text.isNotEmpty()) {
+                displayTextView.text = "Hello, ${findViewById<EditText>(R.id.nameEditText).text}"
+            }
+            else {
+                displayTextView.text = "Please enter our name."
+            }
+
         }
-
-
     }
+
+
 }
